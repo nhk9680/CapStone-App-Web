aaf43b8e18123072d7205bc99ed407fa1bbd65bd
nhk9680 <nhk9680@gmail.com>
2020-06-06 20:39:12 +0900

    minor update

--- cam2web/main.py
+++ cam2web/main.py
@@ -2,8 +2,9 @@
 
 import cv2
 from flask import Flask, render_template, Response
-# ----------------------------------
 # from camera import VideoCamera
+# ----------------------------------
+scale = 0.1 # frame resizing scale
 
 video = cv2.VideoCapture(0)
 app = Flask(__name__)
@@ -20,9 +21,12 @@ def gen():
         #get camera frame
         # frame = camera.get_frame()
 
-        _, frame = video.read()
+        _, frame = video.read() # pass imshow
+
+        frame = cv2.resize(frame, None, fx=scale, fy=scale)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         _, jpeg = cv2.imencode('.jpg', frame)
-        jpeg_byte = jpeg.tobytes()
+        frame = jpeg.tobytes()
 
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
